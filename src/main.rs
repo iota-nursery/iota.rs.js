049d55958bb549de81c3142707c0c1c47f28af6b
Andreas C. Osowski <andreas@osowski.de>
2017-06-22 00:52:01 +0200

    Basic ISS bindings and address generation PoC

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![feature(link_args)]
+
+extern crate iota_sign as sign;
+extern crate iota_trytes as trytes;
+extern crate iota_curl as curl;
+
+pub mod bindings;
+pub use bindings::*;
+
+
+fn main() {
+    /* Intentionally left blank */
+}
